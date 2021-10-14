e1b6daace5ff291522ff88a9b7826f41e51afd37
Bryan Jonas <bryan@crashbang.dev>
2021-10-14 10:29:38 -0500

    Clean up from reorg.

--- collectData/cronfiles/cronscript_after.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-#Move environment variables to file for cron use.
-declare -p | grep -E 'TZ|FROM|TO_1|TO_2|G_KEY' > /container.env
-BASH_ENV=/container.env
-/usr/local/bin/Rscript /home/rstudio/commuteR/collectData/scripts/queryAPI_after.R
